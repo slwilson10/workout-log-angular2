4bc1f71afee4925f1f94f6c46788a4a3eb898fb4
slwilson <slwilson10@gmail.com>
2017-01-25 16:00:25 -0500

    Update dist directory

--- client/dev/config.js
+++ client/dev/config.js
@@ -18,7 +18,7 @@ System.config({
     '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
     '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
     '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-    '@angular/material': 'npm:@angular/material/material.umd.js',
+    '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
 
     // other libraries
     'rxjs': 'npm:rxjs',
